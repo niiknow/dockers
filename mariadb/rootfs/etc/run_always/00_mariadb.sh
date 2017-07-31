816e314fd4614d647301f36a1236d133090a5e33
Tom Noogen <tom@noogen.net>
2017-07-31 14:46:00 -0500

    update base

--- mariadb/rootfs/etc/run_always/00_mariadb.sh
+++ mariadb/rootfs/etc/run_always/00_mariadb.sh
@@ -68,7 +68,7 @@ MYSQL_PASSWORD=${MYSQL_PASSWORD:-""}
 
 tfile=`mktemp`
 if [ ! -f "$tfile" ]; then
-    return 1
+  return 1
 fi
 
 cat << EOF > $tfile
@@ -81,13 +81,13 @@ UPDATE user SET password=PASSWORD("") WHERE user='root' AND host='localhost';
 EOF
 
 if [ "$MYSQL_DATABASE" != "" ]; then
-    echo "[i] Creating database: $MYSQL_DATABASE"
-    echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
+  echo "[i] Creating database: $MYSQL_DATABASE"
+  echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
 
-    if [ "$MYSQL_USER" != "" ]; then
-      echo "[i] Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
-      echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" >> $tfile
-    fi
+  if [ "$MYSQL_USER" != "" ]; then
+    echo "[i] Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
+    echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" >> $tfile
+  fi
 fi
 
 /usr/bin/mysqld --user=mysql --bootstrap --verbose=0 < $tfile
@@ -95,4 +95,3 @@ rm -f $tfile
 
 # finished, stop it for runit
 /usr/bin/mysqladmin shutdown -p"$MYSQL_ROOT_PASSWORD" || true
-
